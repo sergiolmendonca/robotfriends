45a26fbecd7e4d64d3ad1767cab161def473146c
Sergio Junior <sergiopromail@gmail.com>
2022-12-10 13:19:30 -0300

    adding folders organization

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll';
 import './App.css'
 
 class App extends Component {
@@ -25,11 +25,12 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(r => {
-            return r.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
-        if (this.state.robots.length === 0)
+        if (!robots.length)
             return <h1 className='tc f1'>Loading</h1>
 
         return (
